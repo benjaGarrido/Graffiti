b9fb0702c8480dc48baa3a09549508a7c55a0a5e
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2017-01-01 14:23:52 +0100

    adding showLocationServicesDiniedAlert and startLocationManager functions

--- Graffiti/CurrentLocationViewController.swift
+++ Graffiti/CurrentLocationViewController.swift
@@ -40,7 +40,53 @@ class CurrentLocationViewController: UIViewController {
         // Asignamos la imagen del navbar
         let image = UIImage(named: "img_navbar_title")
         self.navigationItem.titleView = UIImageView(image: image)
+        
+        // Siempre que accedemos a la vista la variable debe estar a false
+        updatingLocation = false
+        
     }
 
+    @IBAction func getLocation(_ sender: Any) {
+        startLocationManager()
+    }
+    
+    func startLocationManager() {
+        // Evaluamos en que estado se encuentran los permisos para localizar al usuario
+        let authStatus = CLLocationManager.authorizationStatus()
+        switch authStatus{
+        case .notDetermined:
+            //Cuando no sabemos en que estado está, lo pedimos
+            locationManager.requestWhenInUseAuthorization()
+        case .denied, .restricted:
+            //Mostraremos un mensaje (alert) al usuario
+            showLocationServicesDiniedAlert()
+        default:
+            if CLLocationManager.locationServicesEnabled() {
+                self.updatingLocation = true
+                self.tagButton.isEnabled = false
+                
+                //Creamos los delegados del locationManager
+                locationManager.delegate = self
+                //Definimos el nivel de preción que queremos en la localización
+                locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+                //Pedimos que haga la localizacion
+                locationManager.requestLocation()
+                //Realizamos un zoom en el mapView (definimos una región alrededor de 1km)
+                let region = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, 1000, 1000)
+                mapView.setRegion(mapView.regionThatFits(region), animated: true)
+            }
+        }
+    }
+    
+    func showLocationServicesDiniedAlert() {
+        let alert = UIAlertController(title: "Localización desactivada", message: "Por favor, activa la localización para esta app en Ajustes", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
+extension CurrentLocationViewController: CLLocationManagerDelegate {
+    
+}
