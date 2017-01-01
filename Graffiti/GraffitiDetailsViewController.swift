b88521ed476bed6268475e9e19fd3819bb2a325e
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2017-01-01 23:00:22 +0100

    implementing delegate methods

--- Graffiti/GraffitiDetailsViewController.swift
+++ Graffiti/GraffitiDetailsViewController.swift
@@ -37,6 +37,66 @@ class GraffitiDetailsViewController: UIViewController {
     func configureLabels() {
         lblLatitude.text = String(format: "%.8f", (taggedGraffiti?.coordinate.latitude)!)
         lblLongitude.text = String(format: "%.8f", (taggedGraffiti?.coordinate.longitude)!)
-        lblAddress.text = String(format: "%.8f", (taggedGraffiti?.graffitiAddress)!)
+        lblAddress.text = (taggedGraffiti?.graffitiAddress)!
     }
 }
+
+extension GraffitiDetailsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func takePictureTapped () {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            showPhotoMenu()
+        } else {
+            choosePhotoFromLibrary()
+        }
+    }
+    
+    func showPhotoMenu() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let takePhotoAction = UIAlertAction(title: "Sacar Foto", style: .default) { _ in
+            self.takePhotoWithCamera()
+        }
+        alertController.addAction(takePhotoAction)
+        
+        let chooseFromLibraryAction = UIAlertAction(title: "Elegir de la librería", style: .default) { _ in
+            self.choosePhotoFromLibrary()
+        }
+        alertController.addAction(chooseFromLibraryAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func takePhotoWithCamera() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func choosePhotoFromLibrary() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let imageTaken = info[UIImagePickerControllerEditedImage] as? UIImage
+        imgGraffiti.image = imageTaken
+        btnSave.isEnabled = true
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+
